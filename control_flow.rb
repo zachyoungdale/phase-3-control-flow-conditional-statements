8e87a234e72b5d828b75a4467a184dfb0197796c
Zachary Youngdale <zyoungdale@gmail.com>
2022-10-10 13:16:03 -0400

    commit tree 1030a2efa0f4dcb43c6dc54529764bcac7db17ae

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,42 @@
 def admin_login(username, password)
-  # your code here
+  if username.upcase == "ADMIN" && password == "12345" 
+    return "Access granted"
+  else return "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  if temperature < 40 
+    return "It's brisk out there!"
+  elsif temperature > 40 && temperature < 65
+    return "It's a little chilly out there!"
+  elsif temperature > 85 
+    return "It's too dang hot out there!"
+  else return "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
-  # your code here
+  if num % 3 == 0 && num % 5 == 0 
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else return num
+  end
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  if operation == + 
+    return num1 + num2
+  elsif operation == -
+    return num1 - num2
+  elsif operation == *
+    return num1 * num2
+  elsif operation == / 
+    return num1 / num2
+  else return "Invalid operation!"
+  end
 end
 
